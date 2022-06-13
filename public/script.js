a53ffc58dd9454379fa2d859b8143ac2906fe574
rodrigobosilva <rodrigobosilva@gmail.com>
2022-06-13 12:17:44 +0000

    update 20220613 1217

--- public/script.js
+++ public/script.js
@@ -2,28 +2,30 @@ function loadNav(){
     var myNav = document.getElementById('myNav')
     myNav.innerHTML = 
     `
-    <nav class="nav">
+    <nav>
         <ul class="nav_ul">
             <li class="nav_li">
                 <a class="nav_icon" href="./index.html">
-                    <img src="./img/succulent-icon.png" alt="HOME" style="width: 35px; height: 35px;">
+                    <img src="./img/succulent-icon.png" alt="HOME" style="width: 41px; height: 41px;">
                 </a>
             </li>
             <li class="nav_li">
-                <a class="nav_a nav_active_home" href="./index.html">HOME</a>
+                <a class="nav_padding" href="./index.html">
+                    HOME
+                </a>
             </li>
             <li class="nav_li">
-                <a class="nav_a" href="#">
+                <a class="nav_padding" href="./type.html">
                     TYPE
                 </a>
             </li>
             <li class="nav_li">
-                <a class="nav_a" href="./gallery.html">
+                <a class="nav_padding" href="./gallery.html">
                     GALLERY
                 </a>
             </li>
             <li class="nav_li">
-                <a class="nav_a" href="./articles.html">
+                <a class="nav_padding" href="./articles.html">
                     ARTICLES
                 </a>
             </li>
@@ -31,5 +33,12 @@ function loadNav(){
         </ul>
     </nav>
     `
+}
 
+function loadFooter(){
+    var myNav = document.getElementById('myFooter')
+    myNav.innerHTML = 
+    `
+
+    `
 }
