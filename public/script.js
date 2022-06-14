8b8febdb0cda97b5fc5a922028820e34674ee536
rodrigobosilva <rodrigobosilva@gmail.com>
2022-06-14 11:30:59 +0000

    update 20220614_1130

--- public/script.js
+++ public/script.js
@@ -2,11 +2,11 @@ function loadNav(){
     var myNav = document.getElementById('myNav')
     myNav.innerHTML = 
     `
-    <nav>
+    <nav class="nav_mine">
         <ul class="nav_ul">
             <li class="nav_li">
                 <a class="nav_icon" href="./index.html">
-                    <img src="./img/succulent-icon.png" alt="HOME" style="width: 41px; height: 41px;">
+                    <img src="./img/succulent-icon-white.png" alt="HOME" style="width: 41px; height: 41px;">
                 </a>
             </li>
             <li class="nav_li">
@@ -36,9 +36,13 @@ function loadNav(){
 }
 
 function loadFooter(){
-    var myNav = document.getElementById('myFooter')
-    myNav.innerHTML = 
+    var myFooter = document.getElementById('myFooter')
+    myFooter.innerHTML = 
     `
-
+    <footer>
+        <div class="footer_form">
+            © 2022 ENTA Rodrigo Silva
+        </div>
+    </footer>
     `
 }
